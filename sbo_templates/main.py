c9725456797d7e1ccbd81f3646795e3d8c279d08
Dimitris Zlatanidis <d.zlatanidis@gmail.com>
2015-04-25 02:48:49 +0300

    update change directory

--- sbo_templates/main.py
+++ sbo_templates/main.py
@@ -155,8 +155,8 @@ class SBoTemplates(object):
             self.pwd = self.fields[0].strip()
             if self.pwd and not self.pwd.endswith("/"):
                 self.pwd = self.pwd + "/"
-        self.msg = "Current directory: {0}".format(self.pwd)
-        self.messageBox()
+            self.msg = "Current directory: {0}".format(self.pwd)
+            self.messageBox()
         self.menu()
 
     def mixedform(self):
