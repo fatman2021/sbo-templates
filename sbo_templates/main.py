66d3ee5e7b4448f9d122e8753b3eee16225ac304
Dimitris Zlatanidis <d.zlatanidis@gmail.com>
2015-04-25 22:23:32 +0300

    fix .info handler height

--- sbo_templates/main.py
+++ sbo_templates/main.py
@@ -299,6 +299,7 @@ class SBoTemplates(object):
         """
         self.filename = "{0}.info".format(self.app)
         self.width = 90
+        self.height = 20
         self.comments = self.filename
         field_length = 90
         input_length = 90
